705111d94bd1e021854758195c05570f634e364b
dharwinrvj <dharwinruppa1422@gmail.com>
2022-11-20 19:30:36 +0530

    ibm

---
+++ final-deliverables/code-ibm-deployment/flask/app_ibm.py
@@ -0,0 +1,51 @@
+import numpy as np
+from flask import Flask, render_template, request, redirect, jsonify
+from markupsafe import escape
+import pickle
+import inputScript   #inputScript file - to analyze the URL
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey":API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
+mltoken = token_response.json()["access_token"]
+
+header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
+
+app = Flask(__name__)
+
+# user-inputs the URL in this page
+@app.route('/')
+def predict():
+    return render_template("final.html")
+
+#  fetches given URL and passes to inputScript
+@app.route('/predict',methods=["POST"])
+def y_predict():
+    url = request.form['url']
+    check_predic = inputScript.main(url)
+
+    payload_scoring = {"input_data": [{"field": 'check_predic', "values": check_predic}]}
+    response_scoring = requests.post(os.getenv('DEPLOYMENT_LINK'), json=payload_scoring,headers={'Authorization': 'Bearer ' + mltoken})
+
+    predic = response_scoring.json()
+
+    result = predic['predictions'][0]['values'][0][0]
+
+    print(result)
+
+    # result = predic[0]
+    if(result==-1):
+        pred = "You are safe!! This is a Legimate Website :)"
+    elif(result==1):
+        pred = "You are in a phishing site. Dont Trust :("
+    else:
+        pred = "You are in a suspecious site. Be Cautious ;("
+
+    return render_template("final.html", pred_text = '{}'.format(pred), url = url)
+
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0', debug=True)
---
+++ final-deliverables/code-local-deployment/flask/app.py
@@ -0,0 +1,51 @@
+import numpy as np
+from flask import Flask, render_template, request, redirect, jsonify
+from markupsafe import escape
+import pickle
+import inputScript   #inputScript file - to analyze the URL
+import os
+from pathlib import Path
+
+app = Flask(__name__)
+
+# current_directory = Path(".")
+# file = os.path.join(current_directory,'phishing_website.pkl')
+
+model = pickle.load(open('phishing_website.pkl','rb'))
+
+# user-inputs the URL in this page
+@app.route('/')
+def predict():
+    return render_template("final.html")
+
+#  fetches given URL and passes to inputScript
+@app.route('/predict',methods=["POST"])
+def y_predict():
+    url = request.form['url']
+    check_predic = inputScript.main(url)
+    predic = model.predict(check_predic)
+
+    # print(check_predic)
+    # print (predic)
+    # result = predic[0]
+    
+    if(predic==-1):
+        pred = "You are safe!! This is a Legimate Website :)"
+    elif(predic==1):
+        pred = "You are in a phishing site. Dont Trust :("
+    else:
+        pred = "You are in a suspecious site. Be Cautious ;("
+
+    return render_template("final.html", pred_text = '{}'.format(pred), url = url)
+
+#  takes ip parameters from URL by inputScript and returns the predictions
+@app.route('/predict_api', methods = ['POST'])
+def predict_api():
+
+    data = request.get_json(force = True)
+    predic = model.y_predict([np.array(list(data.values()))])
+    result = predic[0]
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0', debug=True)
